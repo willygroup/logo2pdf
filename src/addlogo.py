80d9e934d545affd247669abfacb2e50a6586da6
Daniele Forti <d.forti@affidaty.io>
2021-06-04 18:13:40 +0200

    minor refactoring

--- src/addlogo.py
+++ src/addlogo.py
@@ -4,6 +4,12 @@
 import os, sys, filetype, requests
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
+debug_trace = True
+
+def log(msg):
+    if debug_trace:
+        print(">>"+msg)
+
 def create_watermark(input_pdf, output, watermark):
     watermark_obj = PdfFileReader(watermark)
     watermark_page = watermark_obj.getPage(0)
@@ -28,20 +34,11 @@ def createDirectories():
     if not os.path.exists('files/logo'):
         os.makedirs('files/logo')
     if not os.path.exists('files/logo.pdf'):
-        print('No logo.pdf file found!')
-        print('Trying to download from web...')
-        url = 'https://github.com/willygroup/addpdflogo-py/raw/main/src/logo.pdf'
-        r = requests.get(url)
-        if(r.status_code == requests.codes.ok):
-            with open('files/logo.pdf', 'wb') as f:
-                f.write(r.content)
-        else:
-            print('Unable to download the example logo.pdf')
-            sys.exit(1)
+        log('No files/logo.pdf file found!')
+        sys.exit(1)
     kind = filetype.guess('files/logo.pdf')
     if kind is None or kind.mime != 'application/pdf':
-        print('logo.pdf is not a valid pdf file!')
-        print(kind.mime)
+        log('logo.pdf is not a valid pdf file! mimetype: '+kind.mime)
         sys.exit(1)
 
 if __name__ == '__main__':
@@ -54,26 +51,28 @@ if __name__ == '__main__':
         try:
             for f in os.listdir("files/nologo"):
                 if f.endswith(".pdf"):
-                    print(os.path.join("files/nologo", f))
-                    print(os.path.join("files/logo", f))
+                    log(os.path.join("files/nologo", f))
+                    log(os.path.join("files/logo", f))
                     o_filename = os.path.join("files/nologo", f)
                     n_filename = os.path.join("files/logo", f)
                     
                     create_watermark(
                         input_pdf=o_filename, 
                         output=n_filename,
-                        watermark='files/logo.pdf')
+                        watermark='files/logo.pdf'
+                    )
             
                     os.remove(o_filename)
                     n_files=n_files+1
 
         except:
-            print("An exception occurred ")
+            log("An exception occurred ")
+            sys.exit(1)
 
         if n_files > 0:
-            print("%d files processed" % n_files)
+            log("%d files processed" % n_files)
         else:
-            print("No file processed")
+            log("No file processed")
     elif len(sys.argv) > 1:
         print (sys.argv)
         print (type(sys.argv))
@@ -82,17 +81,15 @@ if __name__ == '__main__':
         print (files )
         for f in files:
             if f.endswith(".pdf"):
-                print(os.path.join("./", f))
-                #~ print(os.path.join("files/logo", f))
+                log(os.path.join("./", f))
                 o_filename = os.path.join("./", f)
                 n_filename = os.path.join("files/logo", f)
 
                 create_watermark(
                     input_pdf=o_filename, 
                     output=n_filename,
-                    watermark='files/logo.pdf')
+                    watermark='files/logo.pdf'
+                    )
                 n_files=n_files+1
-        
-
 
 # cSpell:ignore nologo
