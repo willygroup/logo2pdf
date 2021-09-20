3f1fa87a3013ca25507cf0d28dbef5bebf5de3be
Daniele Forti <willygroup2@gmail.com>
2021-09-20 14:44:49 +0200

    fixed create python env from windows script

--- create_python_venv_win.bat
+++ create_python_venv_win.bat
@@ -1,4 +1,4 @@
-python -m venv .win_env & .win_env/Script/activate & pip install -r requirements.txt
+python -m venv .win_env &  .\.win_env\Scripts\activate & python -m pip install -r requirements.txt
 
 @echo rem  cp .\.win_env\Lib\site-packages\shiboken2\shiboken2.abi3.dll .\.win_env\Lib\site-packages\PySide2\ 
 @echo rem mkdir .\dist\main\barcode\fonts\ 
